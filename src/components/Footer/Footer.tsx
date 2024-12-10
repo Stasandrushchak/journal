72a555498e8bef4cda49a05d98d33b7eb1cee833
Stasandrushchak <stanislavandr@gmail.com>
2024-12-10 21:15:02 +0300

    add few details

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,6 +3,6 @@ import '../Footer/Footer.css';
 
 export const Footer: React.FC = () => (
   <div className="footer__container" id="phrase">
-    <h2 className="footer__title">"Entertaining Content Broadcast Channel" (ECBC) <br/> 2024 (c) All rights reserved Karabass production</h2>
+    <h2 className="footer__title">"Entertaining Content Broadcast Channel" (ECBC) <br/> 2024 (c) All rights reserved. Artist production.</h2>
   </div>
 );
--- src/components/StartPage/StartPage.tsx
+++ src/components/StartPage/StartPage.tsx
@@ -6,19 +6,19 @@ export const StartPage: React.FC = () => (
     <h1 className="greetings">Entertaining Content Broadcast Channel</h1>
     <h2 className="advice">... Chill & Enjoy</h2>
     <div className="nav-container">
-      <div className="icon-container">
+      <div className="icon__container">
         <a className='icon__link'  href="#photo">
           <img className="icon" src="/images/icons/photo.gif" alt="photo" />
           <h3 className='icon__title'>photo of the day</h3>
         </a>
       </div>
-      <div className="icon-container">
+      <div className="icon__container">
         <a className='icon__link' href="#song">
           <img className="icon" src="/images/icons/song.gif" alt="song icon" />
           <h3 className='icon__title'>song of the day</h3>
         </a>
       </div>
-      <div className="icon-container">
+      <div className="icon__container">
         <a  className='icon__link' href="#joke">
           <img
             className="icon"
@@ -28,14 +28,14 @@ export const StartPage: React.FC = () => (
           <h3 className='icon__title'>todays joke</h3>
         </a>
       </div>
-      <div className="icon-container">
+      <div className="icon__container">
         <a  className='icon__link' href="#phrase">
           <img
             className="icon"
-            src="/images/icons/phrase.png"
+            src="/images/icons/phrase.gif"
             alt="phrase of wisdom"
           />
-          <h3 className='icon__title'>todays mood</h3>
+          <h3 className='icon__title'>Words of wisdom</h3>
         </a>
       </div>
     </div>
